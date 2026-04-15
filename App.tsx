21f3c532816b9a704a02a64cae6039f45ef5da93
Oleksii Pavliuk <141736751+OleksiiPavliuk2002@users.noreply.github.com>
2026-04-15 14:16:06 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import SplashScreen from './src/screens/SplashScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import WishCardScreen from './src/screens/WishCardScreen';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { initDB } from "./src/database/db";
+import HomeScreen from "./src/screens/HomeScreen";
+import WishCardScreen from "./src/screens/WishCardScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    initDB().catch(console.error);
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
+    <SafeAreaView style={{ flex: 1 }} edges={["bottom"]}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <NavigationContainer>
           <StatusBar style="light" />
           <Stack.Navigator
-            initialRouteName="Splash"
-          screenOptions={{ headerShown: false, gestureEnabled: false }}
-        >
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="WishCard" component={WishCardScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </GestureHandlerRootView>
+            initialRouteName="Home"
+            screenOptions={{ headerShown: false, gestureEnabled: false }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="WishCard" component={WishCardScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
     </SafeAreaView>
   );
 }
